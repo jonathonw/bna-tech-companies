e50dc01cd46174e724bea0623de2da73e29f484b
Jonathon Williams <jwilliams@isis.vanderbilt.edu>
2015-01-16 01:02:51 -0600

    Add table sorting; improve HTML sanitization

---
+++ js/my.js
@@ -0,0 +1,29 @@
+var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+
+var tagOrComment = new RegExp(
+    '<(?:'
+    // Comment body.
+    + '!--(?:(?:-*[^->])*--+|-?)'
+    // Special "raw text" elements whose content should be elided.
+    + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
+    + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+    // Regular name
+    + '|/?[a-z]'
+    + tagBody
+    + ')>',
+    'gi');
+function removeTags(html) {
+  var oldHtml;
+  do {
+    oldHtml = html;
+    html = html.replace(tagOrComment, '');
+  } while (html !== oldHtml);
+  return html.replace(/</g, '&lt;');
+}
+
+function noTagsSort(a, b) {
+  aText = removeTags(a);
+  bText = removeTags(b);
+
+  return aText.localeCompare(bText);
+}
